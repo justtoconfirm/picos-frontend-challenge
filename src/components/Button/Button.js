aa1841b3aa75f34cd5bc1397f5b42336a48d62e1
justtoconfirm <office@justtoconfirm.co.uk>
2022-01-23 20:21:05 +0000

    Extend the Button component to render as a link or button element if value is passed into the url prop

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -7,12 +7,23 @@ const Button = props => {
 	const {
 		type = 'button',
 		onClick,
-		children
+		children,
+		url = '',
 	} = props;
+
+	// Check if the component has the url prop value that match with this list so a link can render as a link
+	const isAnchorLink = url && (url.includes('http') || url.startsWith('#') || url.startsWith('mailto') || url.startsWith('/'));
+
+	const renderAsLink = () =>
+		<a href={url} role="button">{children}</a>
+
+	const renderAsButton = () =>
+		<button {...{type, onClick}}>{ children }</button>
 
 	return (
 
-		<button {...{type, onClick}}>{ children }</button>
+		// If the button component has a url prop with a value call the method to render as a link, or render as a button element
+		isAnchorLink ? renderAsLink() : renderAsButton()
 
 	);
 };
